ffef9351ecc91a2db985b366632993cf5c5068f1
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-06-19 22:43:49 +0530

    update index.js

--- Mongose/index.js
+++ Mongose/index.js
@@ -1,4 +1,21 @@
 const mongoose = require("mongoose");
 
+main()
+  .then((res) => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
 // database coonection
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+// create Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+// creating Model(class)
+const User = mongoose.model("User", userSchema);
